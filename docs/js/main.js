7e18abb13e89bf18bfd75cfd6e8fad1081ad246b
PurTahan <sina.farahabadi@gmail.com>
2018-08-24 10:15:27 +0430

    بهبود راست به چپ

--- docs/js/main.js
+++ docs/js/main.js
@@ -24,4 +24,41 @@
 	//Activate bootstrip tooltips
 	$("[data-toggle='tooltip']").tooltip();
 
+	var Dtable = $('.dataTable').dataTable({
+        "language": {
+            "sEmptyTable": "هیچ داده ای در جدول وجود ندارد",
+            "sInfo": "نمایش _START_ تا _END_ از _TOTAL_ رکورد",
+            "sInfoEmpty": "نمایش 0 تا 0 از 0 رکورد",
+            "sInfoFiltered": "(فیلتر شده از _MAX_ رکورد)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ",",
+            "sLengthMenu": "نمایش _MENU_ رکورد",
+            "sLoadingRecords": "در حال بارگزاری...",
+            "sProcessing": "در حال پردازش...",
+            "sSearch": "جستجو:",
+            "sZeroRecords": "رکوردی با این مشخصات پیدا نشد",
+            "oPaginate": {
+                "sFirst": "ابتدا",
+                "sLast": "انتها",
+                "sNext": "بعدی",
+                "sPrevious": "قبلی"
+            },
+            "oAria": {
+                "sSortAscending": ": فعال سازی نمایش به صورت صعودی",
+                "sSortDescending": ": فعال سازی نمایش به صورت نزولی"
+            }
+        },
+        columnDefs: [
+            { orderable: false, targets: -1 }
+        ],
+        "order": [],
+        stateSave: true
+    });
+    Dtable.api().columns().every(function () {
+        var that = this;
+        $('input', this.footer()).on('keyup change', function () {
+            that.search(this.value).draw();
+        });
+    });
+
 })();
